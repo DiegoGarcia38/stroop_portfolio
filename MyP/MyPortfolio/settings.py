c55a5506f3c9676f4ffef4a00968e8ca94d99643
strooplab <jdgarciao1@miusbctg.edu.co>
2024-11-26 15:38:52 -0500

    "Removed .gitignore file, updated settings.py, and deleted Procfile"

--- MyP/MyPortfolio/settings.py
+++ MyP/MyPortfolio/settings.py
@@ -133,15 +133,15 @@ USE_TZ = True
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-    os.path.join(BASE_DIR, 'media/')
+    BASE_DIR, 'static/',
+    BASE_DIR, 'media/'
 ]
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles/"
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "mediafiles/"
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/mediafiles/'
 
 
 # Default primary key field type
