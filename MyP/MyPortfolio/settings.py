1a47fae2fc6ea2b695cdac841999971ff20f96f9
strooplab <jdgarciao1@miusbctg.edu.co>
2024-11-27 23:24:22 -0500

    "Commented out email settings in settings.py and added djangorestframework to requirements.txt"

--- MyP/MyPortfolio/settings.py
+++ MyP/MyPortfolio/settings.py
@@ -101,12 +101,12 @@ DATABASES = {
 #    }
 #}
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp-mail.outlook.com'
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+#EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 # Password validation
