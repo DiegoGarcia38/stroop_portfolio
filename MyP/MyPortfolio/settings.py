2b49ecfe6feab1c5b40ea0eda771171b9fd013c1
DiegoGarcia38 <jdgo45451@gmail.com>
2025-01-21 13:25:24 -0500

    Updated settings.py

--- MyP/MyPortfolio/settings.py
+++ MyP/MyPortfolio/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG') == 'True'
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
