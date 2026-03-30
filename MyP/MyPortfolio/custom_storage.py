639ce7d6dce4f979e838815038bbd0f1cc406da1
strooplab <strooplab@outlook.com>
2026-03-30 00:23:41 -0500

    Fix: Bad manage of existing files

--- MyP/MyPortfolio/custom_storage.py
+++ MyP/MyPortfolio/custom_storage.py
@@ -1,5 +1,16 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+from botocore.exceptions import ClientError
 
-class CustomStorage(S3Boto3Storage):
-    location = 'uploads' 
+
+class R2Storage(S3Boto3Storage):
     file_overwrite = False
+
+    def exists(self, name):
+        try:
+            return super().exists(name)
+        except ClientError:
+            return False
+
+
+class CustomStorage(R2Storage):
+    location = 'uploads'
--- MyP/MyPortfolio/settings.py
+++ MyP/MyPortfolio/settings.py
@@ -164,7 +164,7 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_REGION_NAME = 'auto'
 
 # Usar R2 para archivos subidos (media)
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'MyPortfolio.custom_storage.R2Storage'
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 
